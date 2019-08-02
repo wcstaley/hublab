2d467cee055d9b1abff170a53e66b07e94fff4a4
william staley <wstaley@gtxcel.com>
2019-08-02 11:35:58 -0400

    added wp config local

--- wp-config-local.php
+++ wp-config-local.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'hublabwp' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'hublab' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'PwdDd5OGsFx03ejM' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
